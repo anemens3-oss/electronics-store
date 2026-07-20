e2f50878bc0282c715d882e1e0d16ff9fbc4ee63
anemens3-oss <anemens3@gmail.com>
2026-07-20 08:02:32 +0000

    Commit-login

--- JavaScript/file.js
+++ JavaScript/file.js
@@ -1,3 +1,7 @@
 document.getElementById("loginBtn").addEventListener("click", () => {
     window.location.href = "../log in/index-log-in.html";
 });
+
+document.getElementById("home").addEventListener("click", () => {
+    window.location.href = "../log in/index-log-in.html";
+});
